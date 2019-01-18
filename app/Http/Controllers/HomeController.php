ad0ee3eb6edf3ef297c1a948feecb1779ecf4412
keke1210 <44743904+keke1210@users.noreply.github.com>
2019-01-18 11:43:34 +0100

    Krijova rolet e userave

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +26,14 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //  $role = Role::create(['name' => 'admin']);
+        //  User::find(1)->assignRole('admin');
+        // $role = Role::create(['name' => 'menaxher']);
+        // User::find(2)->assignRole('menaxher');
+        // $role = Role::create(['name' => 'ekonomist']);
+        // User::find(3)->assignRole('ekonomist');
+        //  $role = Role::create(['name' => 'kamarier']);
+        //  User::find(4)->assignRole('kamarier');
         return view('home');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+{{-- Ktu do jete nje kusht if qe nqs je loguar te zhduket ky divi --}}
+@if(Auth::guest())
 <div class="jumbotron text-center">
         <h1>Bar Managment System</h1>
         <p>This is the web for managing your Lounge Bar</p>
@@ -9,4 +12,17 @@
             <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
         </p>
     </div>  
+@endif
+{{--mbaron if-i --}}
+@if(!Auth::guest())
+    <div class="jumbotron text-center">
+        <h1>Kamareri ?filan? {{-- $kamarier->auth()->emer --}} logged in</h1>
+    </div>
+@endif  
+
+    <div class="jumbotron text-center">
+            <h1>?Orari Kamarier-ve?</h1>
+            <p>......</p>
+        </div> 
+        
 @endsection
